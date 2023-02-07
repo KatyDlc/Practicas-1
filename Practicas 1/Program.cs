ea28f0977ef96fe83f9189aad77af21687d1590e
HP <HP@LAPTOP-MITOEF3K>
2023-02-07 17:59:29 -0400

    .

--- Practicas 1/Program.cs
+++ Practicas 1/Program.cs
@@ -4,12 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// 0- Realizar programa que despligue el factorial de numero
+/* Esta exclamación quiere decir que hay que multiplicar todos los números enteros positivos
+que hay entre ese número y el 1. */
+
 namespace Practicas_1
 {
-    internal class Program
+    internal class Program0
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Por favor introduzca el numero que desea desplegar: ");
+            int valor = Convert.ToInt32(Console.ReadLine());
+
+            int factorial = 1;
+
+            for (int i = 1; i <= valor; i++) 
+            {
+                factorial = factorial * i;
+            }
+
+            Console.WriteLine("El factorial de su numero es: " + factorial);
+            Console.ReadLine(); 
         }
     }
 }
